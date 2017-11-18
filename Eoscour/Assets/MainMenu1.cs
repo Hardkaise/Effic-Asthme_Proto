20f5eb336c7330840f5dbeccdac81a8c64cc00f4
VianneyDoleans <vianney.doleans@epitech.eu>
2017-11-18 12:18:54 +0100

    Mainmenu

---
+++ Eoscour/Assets/MainMenu1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu1 : MonoBehaviour
+{
+    void Entrainement(string entrainement)
+    {
+        SceneManager.LoadScene(entrainement);        
+    }
+
+    void Quitter(int layer)
+    {
+        /*#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else*/
+            Application.Quit();
+        //#endif
+    }
+}
