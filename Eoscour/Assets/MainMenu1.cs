923a7736c32fced64ec087d965b235b88047eb00
VianneyDoleans <vianney.doleans@epitech.eu>
2017-11-18 12:39:17 +0100

    corrections

--- Eoscour/Assets/MainMenu1.cs
+++ Eoscour/Assets/MainMenu1.cs
@@ -1,21 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu1 : MonoBehaviour
 {
-    void Entrainement(string entrainement)
+    public void Entrainement(string entrainement)
     {
         SceneManager.LoadScene(entrainement);        
     }
 
-    void Quitter(int layer)
+    public void Quitter(int layer)
     {
-        /*#if UNITY_EDITOR
-            UnityEditor.EditorApplication.isPlaying = false;
-        #else*/
             Application.Quit();
-        //#endif
     }
 }
