94fd37a37a407918c659e5fc456fb7287ad6b131
Hugo Willaume <hugo.willaume@epitech.eu>
2017-11-18 14:20:43 +0100

    Got something to drag an object

---
+++ Eoscour/Assets/ItemMover.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemMover : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void TouchDrag ()
+    {
+        if (Input.touchCount > 0 && (Input.GetTouch(0).phase == TouchPhase.Moved || Input.GetTouch(0).phase == TouchPhase.Stationary))
+        {
+            Debug.Log("ANDROID TOUCH DETECTED");
+            Vector3 finger = Input.GetTouch(0).position;
+            Vector3 touchDeltaPosition = Camera.main.ScreenToWorldPoint(finger);
+
+            Vector2 touchPosWorld2D = new Vector2(touchDeltaPosition.x , touchDeltaPosition.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(touchPosWorld2D, Camera.main.transform.forward);
+            if (hit.collider != null)
+            {
+                Debug.Log(String.Format("Object is in {0}\nFinger touch in {1}\nDeltaTouched in {2}",
+                    transform.position, finger, touchDeltaPosition));
+                if (hit.collider.gameObject == this.gameObject)
+                {
+                    touchDeltaPosition.z = transform.position.z;
+                    Debug.Log(String.Format("Raycast from {0} to {1}", transform.position, touchDeltaPosition.normalized));
+                    //transform.position = Vector3.Lerp(transform.position, touchDeltaPosition, Time.deltaTime);
+                    transform.position = touchDeltaPosition;
+                }
+            }
+
+        }
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+	    TouchDrag();
+	}
+}
---
+++ Eoscour/Assets/TestButtonScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestButtonScript : MonoBehaviour
+{
+    private float shakingDuration = 0;
+    public float shakingThreshold = 0.5f;
+
+    public static float LowPassKernelWidthInSeconds = 1;
+// The greater the value of LowPassKernelWidthInSeconds, the slower the filtered value will converge towards current input sample (and vice versa).
+//    You should be able to use LowPassFilter() function instead of avgSamples().
+
+    public static float AccelerometerUpdateInterval = 1 / 60;
+    private float LowPassFilterFactor = AccelerometerUpdateInterval / LowPassKernelWidthInSeconds;
+
+    private Vector3 lowPassValue = Vector3.zero; // should be initialized with 1st sample
+
+    private Vector3  PhoneAcc;
+    private Vector3 PhoneDeltaAcc;
+
+    private AudioSource sound;
+
+	void Start ()
+	{
+	    sound = GetComponent<AudioSource>();
+	}
+
+    Vector3 LowPassFilter(Vector3 newSample) {
+        lowPassValue = Vector3.Lerp(lowPassValue, newSample, LowPassFilterFactor);
+        return lowPassValue;
+    }
+
+    bool IsShaking()
+    {
+        return Mathf.Abs(PhoneDeltaAcc.x) >= .2;
+    }
+
+    void FixedUpdate () {
+
+        PhoneAcc = Input.acceleration;
+        PhoneDeltaAcc = PhoneAcc - LowPassFilter(PhoneAcc);
+
+        if (IsShaking())
+        {
+            shakingDuration += Time.fixedDeltaTime;
+        }
+        else if (!IsShaking())
+        {
+            shakingDuration = 0;
+        }
+        if (shakingDuration > shakingThreshold)
+        {
+            sound.Play(0);
+            shakingDuration = 0;
+        }
+    }
+
+}
